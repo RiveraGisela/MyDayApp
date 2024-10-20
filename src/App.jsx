7047d4fd07b52d2f6f8c7ae7813ee9fbc15a315e
RiveraGisela <giselarivera1812@gmail.com>
2024-10-19 20:12:30 -0500

    se realizo el componente de task input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import TaskInput from './TaskInput/TaskInput'
 
 function App() {
   
 
   return (
     <>
-<h1>efwefwefewf</h1>
+<TaskInput></TaskInput>
     </>
   )
 }
---
+++ src/TaskInput/TaskInput.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const TaskInput = ({ addTask }) => {
+    const [input, setInput] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (input.trim()) {
+            addTask(input);
+            setInput('');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text" 
+                placeholder="Type new todo" 
+                value={input} 
+                onChange={(e) => setInput(e.target.value)}
+                autoFocus 
+            />
+        </form>
+    );
+};
+
+export default TaskInput;
